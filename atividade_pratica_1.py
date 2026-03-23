69f82c7cfb1c36d66b94cdd29f4fe6d9fd085f9a
Lanna <lannadosanjoscosta@gmail.com>
2026-03-23 10:28:43 -0300

    add metodos declarados

--- atividade_pratica_1.py
+++ atividade_pratica_1.py
@@ -17,19 +17,20 @@ class Normalizador:
     # fazer transform e inverso_transform
     
     def __init__(self):
-        self.parametros_minmax = {}
-        self.mapeamento_labels = {}
-        self.categorias_onehot = {}
-        # p reverter tem que guardar o --> mínimo e máx <-- de cada coluna
+        self.parametros_minmax = {} # p reverter tem que guardar o --> mínimo e máx <-- de cada coluna
+        self.mapeamento_labels = {} # p reverter tem q guardar o mapeamento e o inverso, --> dicionario de categorias <--
+        self.categorias_onehot = {} # p voltar precisa saber quais colunas pertencem ao grupo origina # guarda a --> lista de categorias <--
+        
+    # /\/\/\/\ min max /\/\/\/\
     
-    def set_parametros_MinMax(self, df, colunas):
+    def norm_MinMax(self, df, colunas):
         # transforma valores para uma escala de 0 a 1         
         for coluna in colunas:
             minimo = df[coluna].min()
             maximo = df[coluna].max()
             self.parametros_minmax[colunas] = {"min": minimo, "max":maximo}
             
-    def norm_MinMax(self, df, colunas):
+    def tranforma_MinMax(self, df, colunas):
         # fórmula: x_norm = (x - min) / (max - min)
          # n é adequado para a coluan 'sexo'
         resultado = df.copy()
@@ -56,18 +57,34 @@ class Normalizador:
             resultado[coluna] = resultado[coluna] * (minimo * maximo) + minimo
             
             return resultado
+        
+    # /\/\/\/\/\ label encoding /\/\/\/\/\
 
-    def norm_rotulo_LE():
+    def norm_rotulos(self, df, coluna):
         # serve p transformar categorias em números int
-        # p reverter tem q guardar o mapeamento e o inverso, --> dicionario de categorias <--
+        rotulos = sorted(df[coluna].unique())
+        mapeamento = {rotulo: i for i, rotulo in enumerate(rotulos)}
+        self.mapeamento_labels[coluna] = mapeamento
+
+    def transfroma_rotulo_LE(self, df, coluna):
         # ideal p coluna 'sexo'
         pass
+    
+    def inverte_LE(self, df, coluna):
+        pass
 
-    def norm_HE():
+    # /\/\/\/\ one hot encoding /\/\/\/\
+
+    def norm_OHE():
         # tbm é bom p coluna categórica
         # cria uma coluna p cada categoria
-        # p voltar precisa saber quais colunas pertencem ao grupo original
-        # guarda a --> lista de categorias <--
+        
+       pass
+   
+    def transforma_OHE():
+       pass
+   
+    def inverte_OHE():
        pass
 
 
