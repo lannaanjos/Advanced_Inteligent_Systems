7e0358a34f340764d28eada0559c3d09f18bc4b6
Lanna <lannadosanjoscosta@gmail.com>
2026-03-23 09:49:16 -0300

    add explicações

--- atividade_pratica_1.py
+++ atividade_pratica_1.py
@@ -14,15 +14,25 @@ from sklearn.preprocessing import LabelEncoder, OneHotEncoder, MinMaxScaler
 
 # a classe precisa aprender os parâmentros da ida para poder voltar dps
 
-class Humano:
-    def normalizaMinMax(dados):
+class Normalizador:
+    def norm_MinMax(dados):
+        # transforma valores para uma escala de 0 a 1 | fórmula: x_norm = (x - min) / (max - min)
+        # para voltar = x_original = x_norm * (max - min) + min
+        # p reverter tem que guardar o mínimo e máx de cada coluna
+        # n é adequado para a coluan 'sexo'
         scaler = MinMaxScaler(feature_range=(0,1))
         return scaler.fit_transform(dados)
 
-    def normalizaRotulo():
+    def norm_rotulo_LE():
+        # serve p transformar categorias em números int
+        # p reverter tem q guardar o mapeamento e o inverso
+        # ideal p coluna 'sexo'
         pass
 
-    def pontoQuente():
+    def norm_HE():
+        # tbm é bom p coluna categórica
+        # cria uma coluna p cada categoria
+        # p voltar precisa saber quais colunas pertencem ao grupo original
        pass
 
 
