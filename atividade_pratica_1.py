dac3e569a11094777ffcd6ff2d15f870c3b28180
Lanna <lannadosanjoscosta@gmail.com>
2026-03-23 10:57:46 -0300

    anotacoes label encoding

--- atividade_pratica_1.py
+++ atividade_pratica_1.py
@@ -59,19 +59,20 @@ class Normalizador:
             return resultado
         
     # /\/\/\/\/\ label encoding /\/\/\/\/\
+    # funciona criando um dicionário de tradução entre rotulo e numero
 
     def norm_rotulos(self, df, coluna):
         # serve p transformar categorias em números int
-        rotulos = sorted(df[coluna].unique())
-        mapeamento = {rotulo: i for i, rotulo in enumerate(rotulos)}
-        self.mapeamento_labels[coluna] = mapeamento
+        rotulos = sorted(df[coluna].unique()) # olha coluna e pega valores possiveis sem repetir de forma organizada
+        mapeamento = {rotulo: i for i, rotulo in enumerate(rotulos)} # cria um 'codigo' numerico p cada rotulo, ex: F = 1, M = 0
+        self.mapeamento_labels[coluna] = mapeamento # guarda mapa na classe
 
     def transfroma_rotulo_LE(self, df, coluna):
         # ideal p coluna 'sexo'
         resultado = df.copy()
         
         mapa = self.mapeamento_labels[coluna]        
-        resultado[coluna] = resultado[coluna].map(mapa)
+        resultado[coluna] = resultado[coluna].map(mapa) # aplica o dicionario em cada valor
         
         return resultado
     
@@ -79,9 +80,9 @@ class Normalizador:
         resultado = df.copy()
         
         mapa = self.mapeamento_labels[coluna]
-        mapa_invertido = {valor: chave for chave, valor in mapa.items()}
+        mapa_invertido = {valor: chave for chave, valor in mapa.items()} # mapeia inversamente (valores viram chaves)
         
-        resultado[coluna] = resultado[coluna].map(mapa_invertido)
+        resultado[coluna] = resultado[coluna].map(mapa_invertido) # volta ao estado original
         
         return resultado
 
