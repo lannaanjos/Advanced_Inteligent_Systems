07fa577babc794193b4075cb130aa2ae5c7563fd
lanna <lannadosanjoscosta@gmail.com>
2026-04-06 19:47:40 -0300

    mudando requisitos e importando cosas

--- atividades/iris_cluster_treina.py
+++ atividades/iris_cluster_treina.py
@@ -1,3 +1,9 @@
 import pandas as pd
 from sklearn.preprocessing import MinMaxScaler 
 from sklearn.cluster import KMeans # KMenas é um meta estimdor
+from scipy.spatial.distance import cdist
+import pickle
+import matplotlib.pyplot as plt
+import math
+import numpy as np
+
