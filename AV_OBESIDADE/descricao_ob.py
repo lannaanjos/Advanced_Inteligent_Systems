414fb6d1974a7c9a0b57f93e33cb9b579d849b85
Lacci <lannadosanjoscosta@gmail.com>
2026-04-27 20:48:55 -0300

    feat: add centroid description

--- AV_OBESIDADE/descricao_ob.py
+++ AV_OBESIDADE/descricao_ob.py
@@ -4,31 +4,34 @@ from pathlib import Path
 
 CLUSTER_DESTINO_PATH = Path(__file__).parent / "obesidade_cluster.pkl"
 NORM_PATH = Path(__file__).parent / "norm_obesidade.pkl"
+MAIS_COL_PATH = Path(__file__).parent / "col_names.pkl"
 
+# carrega colunas do hot encoded 
+labels_colunas = pickle.load(open(MAIS_COL_PATH, 'rb'))
 
-
-labels_colunas = ["Gender","Age","Height","Weight","family_history_with_overweight",
-                  "FAVC","FCVC","NCP","CAEC","SMOKE","CH2O","SCC","FAF","TUE","CALC","MTRANS"]
+# separa colunas
+colunas_num = ["Age", "Height", "Weight", "FCVC","NCP", "CH2O", "FAF", "TUE"]
 
 cluster_obesidade = pickle.load(open(CLUSTER_DESTINO_PATH, 'rb'))
-
 centroides = pd.DataFrame(cluster_obesidade.cluster_centers_, columns=labels_colunas)
 
-dados_num_norm = centroides.drop(columns=["Gender", "family_history_with_overweight", "FAVC", "CAEC",
-                                   "SMOKE", "SCC", "CALC","MTRANS"])
+colunas_cat = [col for col in centroides.columns if col not in colunas_num]
 
-dados_cat_norm = centroides[["Gender", "family_history_with_overweight", "FAVC", "CAEC",
-                                   "SMOKE", "SCC", "CALC","MTRANS"]]
+# pega cada categoria
+dados_num_norm = centroides[colunas_num]
 
+dados_cat_norm = centroides[colunas_cat]
+
+# inverse   
 normalizador = pickle.load(open(NORM_PATH, 'rb'))
-dados_num = normalizador.inverse_transform(centroides)
+dados_num = normalizador.inverse_transform(dados_num_norm)
 
-dados_num = pd.DataFrame(dados_num, dados_num_norm.columns)
+# pega dados originais
+dados_num = pd.DataFrame(dados_num, columns=dados_num_norm.columns)
 
 dados_cat = pd.get_dummies(dados_cat_norm.round(0).astype(int))
 
-dados_cat.columns = ["Gender", "family_history_with_overweight", "FAVC", "CAEC",
-                                   "SMOKE", "SCC", "CALC","MTRANS"]
+dados_cat.columns = colunas_cat
 
 dados_cluster_ob = dados_num.join(dados_cat)
 
