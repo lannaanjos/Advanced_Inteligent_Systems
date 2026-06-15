4865bf7d5f8a3baf03c5be2289ffa7e964a66c30
Lacci <lannadosanjoscosta@gmail.com>
2026-06-14 23:09:49 -0300

    chore: preparação para última avaliação

---
+++ Avaliacoes/Black_Friday/treinamento.py
@@ -0,0 +1,3 @@
+from pathlib import Path
+
+DATA_PATH = Path(__file__).parent.parent.parent / "Data" / "retail_black_friday_sales_100k.csv" 
---
+++ Avaliacoes/Heart_Failure/treinamento.py
@@ -0,0 +1,3 @@
+from pathlib import Path
+
+DATA_PATH = Path(__file__).parent.parent.parent / "Data" / "heart_failure_clinical_records_dataset.csv" 
